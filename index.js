9f361a788443321b4fac04708291cc2e57e662c2
Ethan Mallove <mallove@fas.harvard.edu>
2019-04-04 15:26:50 -0400

    Dump env to console
    Dump env to console - see if `app.json` env vars are setting

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var util = require('util')
+console.log("index.js:6, process.env = " + util.inspect(process.env));
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
