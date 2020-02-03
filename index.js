1ff2879d7fe91d2b9d9d3de038bb0194c3de2216
Ethan Mallove <mallove@fas.harvard.edu>
2020-02-03 11:43:34 -0500

    Add env vars manually

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ console.log("index.js:6, process.env = " + util.inspect(process.env));
 var url   = require('url');
 var Redis = require('ioredis');
 
+process.env.REDIS_URL = "redis://h:p1a5c9e67cd6ffb23967d919165df93932f3b55dce981ef0a989adb738d977258@ec2-3-225-119-109.compute-1.amazonaws.com:27899";
+process.env.REDIS_URL_STUNNEL = "redis://h:p1a5c9e67cd6ffb23967d919165df93932f3b55dce981ef0a989adb738d977258@ec2-3-225-119-109.compute-1.amazonaws.com:27900";
+
 console.log("EAM trace, index.js:14, process.env.REDIS_URL = " + util.inspect(process.env.REDIS_URL));
 
 /////////////////////////////////////////////
