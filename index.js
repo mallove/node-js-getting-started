4ce40cef942f156b84c1ca6a062be21be17191df
Ethan Mallove <mallove@fas.harvard.edu>
2020-02-03 13:33:58 -0500

    fix hostname() call

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-require('os');
+const os = require('os');
 console.log("EAM trace, index.js:11, os.hostname() = " + util.inspect(os.hostname()));
 
 
