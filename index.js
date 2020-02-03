a1ed51f6048f1169487684930a3416b4caa5553c
Ethan Mallove <mallove@fas.harvard.edu>
2020-02-03 10:59:34 -0500

    Add Redis connection

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const PORT = process.env.PORT || 5000
 var util = require('util')
 console.log("index.js:6, process.env = " + util.inspect(process.env));
 
+var url   = require('url');
+var Redis = require('ioredis');
+
+/////////////////////////////////////////////
+redis_uri = url.parse(process.env.REDIS_URL);
+var redis = new Redis({
+  port: Number(redis_uri.port) + 1,
+  host: redis_uri.hostname,
+  password: redis_uri.auth.split(':')[1],
+  db: 0,
+  tls: {
+    rejectUnauthorized: false,
+    requestCert: true,
+    agent: false
+  }
+});
+/////////////////////////////////////////////
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
