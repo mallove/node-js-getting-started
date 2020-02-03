fd7987e0243b6a72481e998312aaf9b8c1e142ae
Ethan Mallove <mallove@fas.harvard.edu>
2020-02-03 12:24:57 -0500

    parameterize tls in redis config

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 process.env.REDIS_URL = "redis://h:p1a5c9e67cd6ffb23967d919165df93932f3b55dce981ef0a989adb738d977258@ec2-3-225-119-109.compute-1.amazonaws.com:27899";
-process.env.REDIS_URL_STUNNEL = "redis://h:p1a5c9e67cd6ffb23967d919165df93932f3b55dce981ef0a989adb738d977258@ec2-3-225-119-109.compute-1.amazonaws.com:27900";
+//process.env.REDIS_URL_STUNNEL = "redis://h:p1a5c9e67cd6ffb23967d919165df93932f3b55dce981ef0a989adb738d977258@ec2-3-225-119-109.compute-1.amazonaws.com:27900";
 
 var util = require('util')
-console.log("index.js:6, process.env = " + util.inspect(process.env));
+console.log("index.js:9, process.env = " + util.inspect(process.env));
 
 var url   = require('url');
 var Redis = require('ioredis');
@@ -22,17 +22,24 @@ try {
     redis_port = Number(redis_uri.port) + 1;
   }
 
-  var redis = new Redis({
+  var redis_params = {
     port: redis_port,
     host: redis_uri.hostname,
     password: redis_uri.auth.split(':')[1],
-    db: 0,
-    tls: {
+    db: 0
+  };
+
+  // Add security, if requested
+  if (process.env.STUNNEL_ENABLED) {
+    redis_params.tls = {
       rejectUnauthorized: false,
       requestCert: true,
       agent: false
-    }
-  });
+    };
+    redis_params.port = u.port + 1;
+  }
+
+  var redis = new Redis(redis_params);
 
   redis.on("connect", function (msg) {
       console.log("trace, msg = " + util.inspect(msg));
