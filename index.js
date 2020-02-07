62d3eca1239f1aafbeb97b28609044c4b9688be9
Ethan Mallove <mallove@fas.harvard.edu>
2020-02-07 10:13:32 -0500

    fix express.static call

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(express.static(path.join(__dirname, 'public/.well-known/acme-challenge'), {index: true}))
+  .use( 'public/.well-known/acme-challenge', express.static(path.join(__dirname, 'public/.well-known/acme-challenge'), {index: true}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
